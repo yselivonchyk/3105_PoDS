d545db529cc10a034339a140ed42d48d09e22544
Elchin Valiyev <valiyev.elchin@gmail.com>
2015-01-04 17:13:04 +0100

    Java: XML-RPC library

--- archive/README.md
+++
@@ -1,4 +0,0 @@
-3105_PoDS
-=========
-
-Course project of subject "3105 - Principles of Distributed Systems"
--- archive/source/CNet/CalculationNode/CalculationNode/Extentions/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CalculationNode
-{
-	public static class Constants
-	{
-		public static int DefaultRicartServerPort = 30001;
-		//public static string RicartRelativePath = "./ricardAgrawala";
-		public static string DefaultRelativePath = "./Pods";
-	}
-}
