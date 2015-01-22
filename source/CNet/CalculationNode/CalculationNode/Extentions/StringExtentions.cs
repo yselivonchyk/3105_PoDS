b3dc1b2842f7501023559fd0c85c9db4b4e299d8
Eugene Selivonchyk <leto41ogo@yahoo.com>
2015-01-22 19:35:38 +0100

    CNET: ordering bug fix.

--- source/CNet/CalculationNode/CalculationNode/Extentions/StringExtentions.cs
+++ source/CNet/CalculationNode/CalculationNode/Extentions/StringExtentions.cs
@@ -8,8 +8,9 @@
 				return self.Length > arg.Length;
 			for (int i = 0; i < arg.Length; i++)
 			{
-				if (self[i] > arg[i])
-					return true;
+				if (self[i] == arg[i])
+					continue;
+				return self[i] > arg[i];
 			}
 			return false;
 		}
